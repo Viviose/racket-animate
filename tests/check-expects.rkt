f9c5bae7c9fe2f8da7126d1db7f8b158701fb3aa
Joshua Sanchez <joshuaesanch@gmail.com>
2016-01-08 08:49:43 -0600

    Removing unnecessary files, adding .gitignore

--- tests/check-expects.rkt
+++ tests/check-expects.rkt
@@ -1,4 +1,18 @@
 #lang racket
 (require picturing-programs)
-(require "graph.rkt")
-(require "ellipse.rkt")
+(require test-engine/racket-tests)
+
+(require "../graph.rkt")
+(require "../ellipse.rkt")
+
+;Testing Suite for remove-max-or-min
+(check-expect (remove-max-or-min > (list 2 3 4))
+              (list 2 3)
+              )
+(check-expect (remove-max-or-min > (list 6 2 1))
+              (list 2 1)
+              )
+(check-expect (remove-max-or-min > (list 2 3 5 5))
+              (list 2 3 5)
+              )
+;End Testing Suite
